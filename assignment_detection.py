386839f0f295e56652065c64d88318e1463130ef
aravindrameshr <aravindramesh14@gmail.com>
2020-12-06 12:29:17 +0100

    SVMmobilenet

--- assignment_detection.py
+++ assignment_detection.py
@@ -12,6 +12,7 @@ from sklearn.svm import SVC
 from sklearn.decomposition import PCA
 from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
+from sklearn.metrics import plot_confusion_matrix
 
 ## Load the features (requires assignment 1 to be completed)
 if Path('assignment_classification.npy').is_file():
@@ -31,20 +32,21 @@ data["y_all"] = np.concatenate([data["y_train"], data["y_test"]])
 X_train = data["features"]["hog"]["all"]
 y_train = data["y_all"]
 
+data["features"]["cnn"]["all"] = np.concatenate([data["features"]["cnn"]["train"], data["features"]["cnn"]["test"]])
+data["y_all"] = np.concatenate([data["y_train"], data["y_test"]])
+
+
 grid = GridSearchCV(estimator=pipe, param_grid=param_grid, refit=True, verbose=3)
-grid.fit(X_train, y_train)
+grid.fit(data["features"]["hog"]["train"], data["y_train"])
 model = grid.best_estimator_
 
+
 _,(ax1,ax2)=plt.subplots(2,1,figsize=(15,15))
-pipe.fit(data["features"]["hog"]["train"], data["y_train"])
+model.fit(data["features"]["hog"]["train"], data["y_train"])
 plot_confusion_matrix(model, data["features"]["hog"]["test"], data["y_test"], cmap="Blues", ax=ax1)
-pipe.fit(data["features"]["cnn"]["train"], data["y_train"])
-plot_confusion_matrix(model, data["features"]["cnn"]["test"], data["y_test"], cmap="Blues", ax=ax2)
+
 plt.show()
 
-pca  = PCA(n_components=(35))
-clf = SVC(kernel = "rbf", gamma = "scale", C=5)
-model = Pipeline(steps=[("PCA", pca), ("Classifier", clf)])
 
 model.fit(data["features"]["hog"]["all"], data["y_all"])
 print("Model Trained")
@@ -65,6 +67,38 @@ detector.detectVideo(
 	video_capture=cap,
 	write = True,
 	show_video=True,
+	write_fps = 10
+)
+
+grid = GridSearchCV(estimator=pipe, param_grid=param_grid, refit=True, verbose=3)
+grid.fit(data["features"]["cnn"]["train"], data["y_train"])
+model = grid.best_estimator_
+
+_,(ax1,ax2)=plt.subplots(2,1,figsize=(15,15))
+model.fit(data["features"]["cnn"]["train"], data["y_train"])
+plot_confusion_matrix(model, data["features"]["cnn"]["test"], data["y_test"], cmap="Blues", ax=ax2)
+plt.show()
+
+
+model.fit(data["features"]["cnn"]["all"], data["y_all"])
+print("Model Trained")
+# Load video
+cap = cv2.VideoCapture("pedestrian.mp4")
+
+## Exercise 3.2
+# Apply the detection algorithm by constructing a detector (see detector.py)
+# Use the feature type and best performing parameters based on your findings in exercise 3.2
+# Create a HogDescriptor or CnnDescriptor to pass to the Detector and run your classifier on the detection problem
+# #YOUR_CODE_HERE
+classifier = model
+descriptor = CnnDescriptor()
+detector = Detector(classifier, descriptor)
+
+# Apply the detection algorithm to the video and save the result to disk
+detector.detectVideo(
+	video_capture=cap,
+	write = True,
+	show_video=False,
 	write_fps = 10
 )
 
